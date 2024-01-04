835007e4d3b8979d1c43f323d379ba17e8463fac
Pauloper1 <paulcrescent.p@gmail.com>
2024-01-04 23:53:50 +0530

    codeforces practice rating 800

---
+++ codeforces/practice/BoyGirl.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+	map<int, int> myMap;
+
+	for(int i = 97; i <=122; i++){
+		myMap[i] = 0;
+	}
+
+	string name;
+	cin >> name;
+	int sum = 0;
+
+	for(int i = 0; i < name.length(); i++){
+		if(myMap[name[i]] == 0){
+			myMap[name[i]] = 1;
+			// printf("%d\n", sum);
+			sum += 1;
+		} else {
+
+		}
+	}
+
+	printf("%d\n", sum);
+	if(sum%2 != 0){
+		printf("IGNORE HIM");
+	} else {
+		printf("CHAT WITH HER");
+	}
+	
+
+	return 0;
+}
